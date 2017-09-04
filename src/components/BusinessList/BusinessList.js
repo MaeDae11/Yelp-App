cf6f1c3cd3b803d4bd403c1a4c99036d3bc153b5
Katie <katherinelane11@gmail.com>
2017-09-04 17:37:32 -0500

    adds additional content

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,3 +3,15 @@ import ReactDOM from 'react-dom';
 import './BuisnessList.css';
 import Business from '../Business/Business.js';
 
+
+class BusinessList extends Component {
+
+    render() {
+        return(
+            <div>
+            </div>
+        );
+    };
+};
+
+export default BusinessList;
