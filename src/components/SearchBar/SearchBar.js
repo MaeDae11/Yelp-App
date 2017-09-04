9463f71c37f728bc4c8cb95be3dd359c0bcad4de
Katie <katherinelane11@gmail.com>
2017-09-04 18:03:37 -0500

    adds to SearchBar - pictures to css and render to js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,9 +3,9 @@ import './SearchBar.css';
 
 
 const sortByOptions = {
-    'Best Match' : x,
-    'Highest Rated' : y,
-    'Most Reviewed' : z
+    'Best Match' : 'best_match',
+    'Highest Rated' : 'rating',
+    'Most Reviewed' : 'review_count'
 };
 
 class SearchBar extends Component {
@@ -20,7 +20,22 @@ class SearchBar extends Component {
         });
     };
     render(){
-        
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                <ul>
+                    {renderSortByOptions()}
+                </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        )
     }
 };
 
