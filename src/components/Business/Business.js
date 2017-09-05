4d9856ccfcf8a2b15f0e41059be2a89654cfc95a
Katie <katherinelane11@gmail.com>
2017-09-05 15:01:43 -0400

    passes info from parent component to child compoents

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,17 +2,7 @@ import React, {Component} from 'react';
 import './Business.css';
 
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
+
 // The purpose of the <Business /> component is to represent how a business (a restaurant) in Ravenous will be formatted and styled
 class Business extends Component {
 
@@ -23,17 +13,17 @@ class Business extends Component {
                 <div className="image-container">
                     <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
                 </div>
-                <h2>{business.name}</h2>
+                <h2>{this.props.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{business.address}</p>
-                        <p>{business.city}</p>
-                        <p>{business.state}, {business.zipCode}</p>
+                        <p>{this.props.address}</p>
+                        <p>{this.props.city}</p>
+                        <p>{this.props.state}, {this.props.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{business.category}</h3>
-                        <h3 className="rating">Rating: {business.rating}/5</h3>
-                        <p>Reviews: {business.reviewCount}</p>
+                        <h3>{this.props.category}</h3>
+                        <h3 className="rating">Rating: {this.props.rating}/5</h3>
+                        <p>Reviews: {this.props.reviewCount}</p>
                     </div>
                 </div>
             </div>
